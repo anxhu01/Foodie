31ccc6be36c317c1df798bf6b0be691c3dabf140
anxhu01 <sahooanshuman1205@gmail.com>
2026-08-05 23:46:51 +0530

    Initial project setup

---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './PlaceOrder.css'
+const PlaceOrder = () => {
+  return (
+    <form className='place-order'>
+      <div className="place-order-left">
+    <p className="title">Delivery Information</p>
+    <div className="multi-fields">
+      <input type="text" placeholder='First name' />
+      <input type="text" placeholder='Last name' />
+    </div>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
+  )
+}
+
+export default PlaceOrder
